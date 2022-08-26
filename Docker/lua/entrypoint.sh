5901c5cc565db461fcc6fc983860956fdcafe970
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-08-26 13:56:06 +0100

    Add necessary for Python YottaDB binding

--- Docker/lua/entrypoint.sh
+++ Docker/lua/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 apt-get update
-apt-get install -y  lua5.3 liblua5.3-dev git make luarocks
+apt-get install -y  lua5.3 liblua5.3-dev git make luarocks python3 python3-pip python3-dev python3-setuptools libffi-dev
+source /opt/yottadb/current/ydb_env_set
+python3 -m pip install yottadb
+cd /home/sparkline1
+python3 gen.py &
 cd /usr/local
 git clone https://github.com/orbitalquark/lua-yottadb.git
 cd lua-yottadb
@@ -10,7 +14,6 @@ cp _yottadb.so /usr/local/lib/lua/5.3
 cp yottadb.lua /usr/local/lib/lua/5.3
 source $(pkg-config --variable=prefix yottadb)/ydb_env_set
 /opt/yottadb/current/ydb <<< 'H'
-(echo "D ^%GI";echo "/home/sparkline1/vitals.go";echo "") | /opt/yottadb/current/ydb
 luarocks install pegasus
 cd /home/lua
 source /opt/yottadb/current/ydb_env_set
