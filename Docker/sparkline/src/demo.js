da84e7e438943b13af83a2bba5673920f91bf3ef
Raman Sailopal <rsailopal@robinsonbrothers.co.uk>
2022-08-26 13:55:39 +0100

    Added last reading to display

--- Docker/sparkline/src/demo.js
+++ Docker/sparkline/src/demo.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Sparklines, SparklinesBars, SparklinesLine, SparklinesCurve,  SparklinesNormalBand, SparklinesReferenceLine, SparklinesSpots } from '../src/Sparklines';
+import { Sparklines, SparklinesBars, SparklinesLine, SparklinesCurve,  SparklinesNormalBand, SparklinesReferenceLine, SparklinesSpots, SparklinesText } from '../src/Sparklines';
 import axios from 'axios';
 
 
-axios.get(process.env.REACT_APP_APIADD).then(function(json_data) {
+axios.get('http://192.168.240.21:9090/temp').then(function(json_data) {
     var sampleData = [];
 	    var keys = Object.keys(json_data.data);
 	    keys.forEach(function(key){
 		            sampleData.push(json_data.data[key]);
 		        });
+	    var lastval=sampleData[sampleData.length - 1];
     ReactDOM.render(<Sparklines data={sampleData}>
+	            <SparklinesText text="test" style={ {color: "yellow" }}/>
 	            <SparklinesLine />
+	            <SparklinesSpots />
 	            <SparklinesReferenceLine type="mean" />
 	        </Sparklines>
 	            
-	    , document.getElementById('simple'));
+	    , document.getElementById('temp'));
+    ReactDOM.render(<h1>{lastval}</h1>
+
+	                            , document.getElementById('templast'));
+
+
 });
+
+axios.get('http://192.168.240.21:9090/pulse').then(function(json_data) {
+	    var sampleData = [];
+	            var keys = Object.keys(json_data.data);
+	            keys.forEach(function(key){
+			                                sampleData.push(json_data.data[key]);
+			                            });
+	    var lastval=sampleData[sampleData.length - 1];
+	    ReactDOM.render(<Sparklines data={sampleData}>
+		                        <SparklinesLine style={{ fill: "none" }}/>
+		                         <SparklinesReferenceLine type="mean" />
+		                        <SparklinesSpots />
+		                    </Sparklines>
+
+		                , document.getElementById('pulse'));
+             ReactDOM.render(<h1>{lastval}</h1>
+                                , document.getElementById('pulselast'));
+
+});
+
+axios.get('http://192.168.240.21:9090/resp').then(function(json_data) {
+	            var sampleData = [];
+	                    var keys = Object.keys(json_data.data);
+	                    keys.forEach(function(key){
+				                                                            sampleData.push(json_data.data[key]);
+				                                                        });
+	            var lastval=sampleData[sampleData.length - 1];
+	            ReactDOM.render(<Sparklines data={sampleData}>
+			                                            <SparklinesLine />
+			                                            <SparklinesSpots />
+			                                            <SparklinesReferenceLine type="mean" />
+			                                        </Sparklines>
+
+			                                    , document.getElementById('resp'));
+                     ReactDOM.render(<h1>{lastval}</h1>
+
+	                                        , document.getElementById('resplast'));
+
+});
+
+
